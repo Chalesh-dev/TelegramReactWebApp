fa90b059678f4d324452823e92720d627ed8793b
AmirSeraj <amir.seraj6990@gmail.com>
2024-05-30 08:40:49 +0330

    frfrfrfr

--- src/index.tsx
+++ src/index.tsx
@@ -17,76 +17,76 @@ import ScanQrPopupDemo from './ScanQrPopupDemo';
 import ExpandDemo from './ExpandDemo';
 import useBetaVersion from './useBetaVersion';
 
-const DemoApp: FC<{
-  onChangeTransition: DispatchWithoutAction;
-}> = ({ onChangeTransition }) => {
-  const [colorScheme, themeParams] = useThemeParams();
-  const [isBetaVersion, handleRequestBeta] = useBetaVersion(false);
-  const [activeBtn, setActiveBtn] = useState(true);
+// const DemoApp: FC<{
+//   onChangeTransition: DispatchWithoutAction;
+// }> = ({ onChangeTransition }) => {
+//   const [colorScheme, themeParams] = useThemeParams();
+//   const [isBetaVersion, handleRequestBeta] = useBetaVersion(false);
+//   const [activeBtn, setActiveBtn] = useState(true);
 
-  return (
-    <div>
-      <ConfigProvider
-        theme={
-          themeParams.text_color
-            ? {
-                algorithm:
-                  colorScheme === 'dark'
-                    ? theme.darkAlgorithm
-                    : theme.defaultAlgorithm,
-                token: {
-                  colorText: themeParams.text_color,
-                  colorPrimary: themeParams.button_color,
-                  colorBgBase: themeParams.bg_color,
-                },
-              }
-            : undefined
-        }
-      >
-        <header className="App-header">
-          <img
-            onClick={handleRequestBeta}
-            src={logo}
-            className="App-logo"
-            alt="logo"
-          />
-        </header>
-        <div className="contentWrapper">
-          {isBetaVersion && (
-            <div className="betaVersion">
-              <h3>WARNING: BETA VERSION</h3>
-              <button onClick={() => setActiveBtn(state => !state)}>
-                change button
-              </button>
-              <button onClick={onChangeTransition}>change </button>
-            </div>
-          )}
-          <ExpandDemo />
-          {!activeBtn ? (
-            <MainButtonDemo
-              initialValues={{
-                show: isBetaVersion,
-                text: 'SECOND BUTTON',
-                progress: true,
-              }}
-              key="1"
-            />
-          ) : (
-            <MainButtonDemo
-              key="2"
-              initialValues={{
-                show: isBetaVersion,
-              }}
-            />
-          )}
-          <ShowPopupDemo />
-          <HapticFeedbackDemo />
-          <ScanQrPopupDemo />
-        </div>
-      </ConfigProvider>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <ConfigProvider
+//         theme={
+//           themeParams.text_color
+//             ? {
+//                 algorithm:
+//                   colorScheme === 'dark'
+//                     ? theme.darkAlgorithm
+//                     : theme.defaultAlgorithm,
+//                 token: {
+//                   colorText: themeParams.text_color,
+//                   colorPrimary: themeParams.button_color,
+//                   colorBgBase: themeParams.bg_color,
+//                 },
+//               }
+//             : undefined
+//         }
+//       >
+//         <header className="App-header">
+//           <img
+//             onClick={handleRequestBeta}
+//             src={logo}
+//             className="App-logo"
+//             alt="logo"
+//           />
+//         </header>
+//         <div className="contentWrapper">
+//           {isBetaVersion && (
+//             <div className="betaVersion">
+//               <h3>WARNING: BETA VERSION</h3>
+//               <button onClick={() => setActiveBtn(state => !state)}>
+//                 change button
+//               </button>
+//               <button onClick={onChangeTransition}>change </button>
+//             </div>
+//           )}
+//           <ExpandDemo />
+//           {!activeBtn ? (
+//             <MainButtonDemo
+//               initialValues={{
+//                 show: isBetaVersion,
+//                 text: 'SECOND BUTTON',
+//                 progress: true,
+//               }}
+//               key="1"
+//             />
+//           ) : (
+//             <MainButtonDemo
+//               key="2"
+//               initialValues={{
+//                 show: isBetaVersion,
+//               }}
+//             />
+//           )}
+//           <ShowPopupDemo />
+//           <HapticFeedbackDemo />
+//           <ScanQrPopupDemo />
+//         </div>
+//       </ConfigProvider>
+//     </div>
+//   );
+// };
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -97,9 +97,10 @@ const App = () => {
 
   return (
     <WebAppProvider options={{ smoothButtonsTransition }}>
-      <DemoApp
+      <ExpandDemo />
+      {/* <DemoApp
         onChangeTransition={() => setSmoothButtonsTransition(state => !state)}
-      />
+      /> */}
     </WebAppProvider>
   );
 };
