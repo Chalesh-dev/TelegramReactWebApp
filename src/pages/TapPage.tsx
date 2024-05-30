b1fdf701f085d937f57272de03603fc98432f6bf
AmirSeraj <amir.seraj6990@gmail.com>
2024-05-30 10:53:36 +0330

    frfrtt

--- src/pages/TapPage.tsx
+++ src/pages/TapPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TapPage = ({socket, userId} : any) => {
   return (
-    <div style={{color: 'red'}}>TapPage {userId}</div>
+    <div style={{color: 'red'}}>TapPage: {userId}</div>
   )
 }
 
