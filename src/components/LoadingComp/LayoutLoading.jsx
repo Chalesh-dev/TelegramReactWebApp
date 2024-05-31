5182ac4db7c63558729641f95eee8d98f08a15f3
AmirSeraj <amir.seraj6990@gmail.com>
2024-05-31 13:00:23 +0330

    loading

--- src/components/LoadingComp/LayoutLoading.jsx
+++ src/components/LoadingComp/LayoutLoading.jsx
@@ -2,7 +2,7 @@ import "./layoutLoading.css";
 
 const LayoutLoading = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center bg-transparent bg-gradient-to-br from-violet-500 via-purple-600 to-indigo-700">
+    <div className="w-full min-h-[100vh] flex justify-center items-center bg-transparent bg-gradient-to-br from-violet-500 via-purple-600 to-indigo-700">
       <span class="loader"></span>
     </div>
   );
